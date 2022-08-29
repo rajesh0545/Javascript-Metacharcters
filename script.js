a6ee7cb7c29bdb3460afba9209c1cb6639b3c9fa
rajesh <krajeshgowri7032@mail.com>
2022-08-29 18:06:43 +0530

    Javascript-meta

---
+++ script.js
@@ -0,0 +1,24 @@
+
+// let text = "That's hot!";
+// let pattern = /h.t/g;
+// let result = text.match(pattern);
+
+// document.getElementById("meta").innerHTML = result;
+
+
+
+
+
+// let text = "Give 100%!"; 
+// let pattern = /\w/g;
+// let result = text.match(pattern);
+
+// document.getElementById("meta").innerHTML = result;
+
+
+
+let text = "Education is the passport to the furure";
+let pattern = /\S/g;
+let result = text.match(pattern);
+
+document.getElementById("white").innerHTML = result;
